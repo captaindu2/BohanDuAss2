2fd95c27a74d0baf52616f34b2705e69c8d91876
BohaHU <102597307+BohaHU@users.noreply.github.com>
2022-03-30 14:50:22 +0800

    Initial commit

---
+++ Assets/Scripts/HumanManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour
+{
+   public GameObject human;
+   public float value;
+   public Vector3 sizeChange;
+
+
+     public void MoveLeft()
+     {
+        value = value - 0.2f;
+        human.transform.position=new Vector3(value,0,0);
+     }
+     
+
+      public void RotateLeft()
+      {
+            human.transform.Rotate(0f,5f,0f);
+      }
+      
+
+     public void Grow()
+     {
+        human.transform.localScale = human.transform.localScale + sizeChange;
+
+
+     }
+
+      public void ResetHuman()
+      {
+         human.transform.position = new Vector3(0,0,0);
+         human.transform.rotation = Quaternion.Euler(new Vector3(0,0,0));
+         human.transform.localScale = new Vector3(1,1,1);
+      }
+      }
